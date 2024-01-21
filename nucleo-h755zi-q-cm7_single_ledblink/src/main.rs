3d053ac81c64de6b0c5478fdfc5abafc8bfb9130
Kazushige Taniguchi <taniguchi.kazushige@gmail.com>
2024-01-21 16:13:47 +0900

    example of blinking led on cortex-m7 at nucleo-h755zi-q. (not dual core)

---
+++ nucleo-h755zi-q-cm7_single_ledblink/src/main.rs
@@ -0,0 +1,47 @@
+#![deny(warnings)]
+#![no_main]
+#![no_std]
+
+use cortex_m_rt::entry;
+use stm32h7xx_hal::{pac, prelude::*};
+use log::info;
+
+//use rtt_target::{rprintln, rtt_init_print, ChannelMode::BlockIfFull};
+
+#[macro_use]
+mod utilities;
+
+#[entry]
+fn main() -> ! {
+
+    utilities::logger::init();
+    //rtt_init_print!(BlockIfFull);
+    info!("wake cm7");
+
+    let cp = cortex_m::Peripherals::take().unwrap();
+    let dp = pac::Peripherals::take().unwrap();
+
+    // Constrain and Freeze power
+    let pwr = dp.PWR.constrain();
+    let pwrcfg = example_power!(pwr).freeze();
+
+    // Constrain and Freeze clock
+    let rcc = dp.RCC.constrain();
+    let ccdr = rcc.sys_ck(100.MHz()).freeze(pwrcfg, &dp.SYSCFG);
+
+    let gpiob = dp.GPIOB.split(ccdr.peripheral.GPIOB);
+
+    // Configure PE1 as output.
+    let mut led = gpiob.pb0.into_push_pull_output();
+
+    // Get the delay provider.
+    let mut delay = cp.SYST.delay(ccdr.clocks);
+
+    loop {
+        led.set_high();
+        delay.delay_ms(500_u16);
+
+        led.set_low();
+        delay.delay_ms(500_u16);
+    }
+}
