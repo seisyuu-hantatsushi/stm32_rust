508465ebbe162639ea83fea81f29b162fa3a1ef8
TANIGUCHI, Kazushige <taniguchi.kazushige@gmail.com>
2024-06-01 12:46:34 +0900

    the sharedbuffer is divided into those that use critical sections and those that do not.

--- crates/embedded-lib/src/shared_ringbuffer.rs
+++ crates/embedded-lib/src/shared_ringbuffer.rs
@@ -23,39 +23,45 @@ struct SharedRingBufferLayout<const S:usize,const N:usize> {
     start_of_frame: *mut [u8;S],
 }
 
-pub struct SharedRingBuffer<const S:usize,const N:usize, LockFn, UnlockFn>
-where
-    LockFn: FnMut(), UnlockFn: FnMut()
+trait CriticalSection : Drop {
+    fn lock(&self) -> Result<(),SharedRingBufferError>;
+}
+
+pub struct SharedRingBuffer<const S:usize,const N:usize>
 {
     shared_address  : *mut u32,
     buffer_size : u32,
-    lock: Option<LockFn>,
-    unlock: Option<UnlockFn>,
     shared_ringbuffer: &'static mut SharedRingBufferLayout<S, N>,
     shared_ringbuffer_holder: &'static mut [[u8;S]],
 }
 
-impl<const S:usize,const N:usize, LockFn, UnlockFn> SharedRingBuffer<S,N,LockFn,UnlockFn>
+pub struct SharedRingBufferWithCS<const S:usize,const N:usize, CS>
 where
-    LockFn: FnMut(), UnlockFn: FnMut()
+    CS: CriticalSection
+{
+    shared_address  : *mut u32,
+    buffer_size : u32,
+    lock: Option<CS>,
+    shared_ringbuffer: &'static mut SharedRingBufferLayout<S, N>,
+    shared_ringbuffer_holder: &'static mut [[u8;S]],
+}
+
+
+impl<const S:usize,const N:usize> SharedRingBuffer<S,N>
 {
 
     pub unsafe fn assign(shared_address: *mut u32,
-                         buffer_size: u32,
-                         lock_fns: Option<(LockFn, UnlockFn)>) -> Self {
+                         buffer_size: u32) -> Self {
         let shared_ringbuffer_ptr = shared_address as *mut SharedRingBufferLayout<S, N>;
         let mut shared_ringbuffer: &mut SharedRingBufferLayout<S, N> = unsafe { shared_ringbuffer_ptr.as_mut().unwrap() };
         if (buffer_size as usize) < 512+S*N { panic!("memory size is not enough. memory size must be rather than {}", S*N+512); };
         shared_ringbuffer.start_of_frame = (shared_address as *mut u8).add(128) as *mut [u8; S];
         let shared_ringbuffer_holder = slice::from_raw_parts_mut(shared_ringbuffer.start_of_frame, N);
         debug!("{:?}", shared_ringbuffer);
-        let (lock_fn, unlock_fn) = if let Some((lock_fn, unlock_fn)) = lock_fns { (Some(lock_fn),Some(unlock_fn)) } else { (None,None) };
 
         SharedRingBuffer {
             shared_address,
             buffer_size,
-            lock : lock_fn,
-            unlock: unlock_fn,
             shared_ringbuffer,
             shared_ringbuffer_holder
         }
@@ -65,22 +71,14 @@ where
         debug!("head {}, tail {}",
                self.shared_ringbuffer.head,
                self.shared_ringbuffer.tail);
-        if let Some(ref mut lock_fn) = self.lock {
-            (lock_fn)();
-            if let Some(ref mut unlock_fn) = self.unlock {
-                (unlock_fn)();
-            };
+        let next_tail = if self.shared_ringbuffer.tail + 1 >= N { 0 } else { self.shared_ringbuffer.tail + 1 };
+        if next_tail == self.shared_ringbuffer.head {
+            return Err(SharedRingBufferError::NoSpace);
         }
-        else {
-            let next_tail = if self.shared_ringbuffer.tail + 1 >= N { 0 } else { self.shared_ringbuffer.tail + 1 };
-            if next_tail == self.shared_ringbuffer.head {
-                return Err(SharedRingBufferError::NoSpace);
-            }
-            let mut dst_buffer = self.shared_ringbuffer_holder[self.shared_ringbuffer.tail].as_mut_slice();
+        let mut dst_buffer = self.shared_ringbuffer_holder[self.shared_ringbuffer.tail].as_mut_slice();
 
-            dst_buffer[..message.len()].copy_from_slice(message);
-            self.shared_ringbuffer.tail = next_tail;
-        }
+        dst_buffer[..message.len()].copy_from_slice(message);
+        self.shared_ringbuffer.tail = next_tail;
         Ok(())
     }
 
